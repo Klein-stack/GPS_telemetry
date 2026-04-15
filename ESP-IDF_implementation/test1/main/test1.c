3eafc7ce7af80aef7381bef4edce69eff5f3a6bc
Klein-stack <sandeepsnathan08@gmail.com>
2026-04-15 22:59:13 +0530

    added uart driver and config

--- ESP-IDF_implementation/test1/main/test1.c
+++ ESP-IDF_implementation/test1/main/test1.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
-#include "freertos/FreeRTO"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_log.h"
+
+const int ring_buffer_size = (1024*2);
+QueueHandle_t queue_handler;
+
+uart_driver_install(UART_NUM_1, ring_buffer_size, ring_buffer_size, 10, &queue_handler, 0);
+
+uart_config_t uart_configurs = {
+    .baud_rate = 115200,
+    .data_bits = UART_DATA_8_BITS,
+    .stop_bits = UART_STOP_BITS_1,
+    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    .parity = UART_PARITY_DISABLE
+};
+
+uint8_t data[128];
+uart_read_bytes(UART_NUM_1 );
